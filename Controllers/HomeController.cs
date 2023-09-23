7028490304d1ff96b14dc97e9adda3b78f989c64
jonnelmlique <jonnelmlique57@gmail.com>
2023-09-23 12:38:49 +0800

    Student Info

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,8 +7,9 @@ public class HomeController : Controller
         var studentInfoArray = new[]
         {
             new StudentInfoModel { Name = "Jonnel Lique", StudentId = "21-1474", StudentAge = "21" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Rudy Lique", StudentId = "S002", StudentAge = "22" },
+            new StudentInfoModel { Name = "Sonia Lique", StudentId = "S003", StudentAge = "31" },
+            new StudentInfoModel { Name = "John James Lique", StudentId = "S004", StudentAge = "25" }
         };
 
         return View(studentInfoArray);
